8dfcd21f907162242d854ae137a39bcb7a9f4b83
willtack <will.s.tackett@gmail.com>
2021-03-24 16:10:49 -0400

    init

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM willtack/wscore-ct-heatmap:0.0.1
+
+MAINTAINER Will Tackett <William.Tackett@pennmedicine.upenn.edu>
+
+# Install basic dependencies
+RUN apt-get update && apt-get -y install \
+    jq \
+    tar \
+    zip \
+    curl \
+    nano \
+    build-essential
+#RUN rm -f /usr/bin/python && ln -s /usr/bin/python /usr/bin/python3
+
+# Install conda environment
+RUN curl -sSLO https://repo.continuum.io/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh && \
+    bash Miniconda3-4.5.11-Linux-x86_64.sh -b -p /usr/local/miniconda && \
+    rm Miniconda3-4.5.11-Linux-x86_64.sh
+
+ENV PATH=/usr/local/miniconda/bin:$PATH \
+    LANG=C.UTF-8 \
+    LC_ALL=C.UTF-8 \
+    PYTHONNOUSERSITE=1
+
+RUN conda install -y python=3.7.1 \
+    chmod -R a+rX /usr/local/miniconda; sync && \
+    chmod +x /usr/local/miniconda/bin/*; sync && \
+    conda build purge-all; sync && \
+    conda clean -tipsy && sync
+
+# Install python packages
+RUN pip install flywheel-sdk==12.4.0 \
+                heudiconv==0.9.0 \
+                fw-heudiconv==0.3.3 \
+                pybids==0.12.4
+
+# Make directory for flywheel spec (v0)
+ENV FLYWHEEL /flywheel/v0
+RUN mkdir -p ${FLYWHEEL}
+#COPY run ${FLYWHEEL}/run
+COPY run.py ${FLYWHEEL}/run.py
+COPY manifest.json ${FLYWHEEL}/manifest.json
+RUN chmod a+rx ${FLYWHEEL}/*
+
+# Set the entrypoint
+ENTRYPOINT ["/flywheel/v0/run.py"]
+
+# ENV preservation for Flywheel Engine
+RUN env -u HOSTNAME -u PWD | \
+  awk -F = '{ print "export " $1 "=\"" $2 "\"" }' > ${FLYWHEEL}/docker-env.sh
+
+WORKDIR /flywheel/v0
---
+++ run.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import logging
+from pathlib import PosixPath
+import flywheel
+
+print(sys.path)
+
+# logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('heatmap-gear')
+logger.info("=======: w-score HeatMap :=======")
+
+with flywheel.GearContext() as context:
+    # Setup basic logging
+    context.init_logging()
+    config = context.config
+    analysis_id = context.destination['id']
+    gear_output_dir = PosixPath(context.output_dir)
+    run_script = gear_output_dir / "heatmap_run.sh"
+    output_root = gear_output_dir / analysis_id
+    working_dir = PosixPath(str(output_root.resolve()) + "_work")
+
+    # Get relevant container objects
+    fw = flywheel.Client(context.get_input('api_key')['key'])
+    analysis_container = fw.get(analysis_id)
+    project_container = fw.get(analysis_container.parents['project'])
+    session_container = fw.get(analysis_container.parent['id'])
+    subject_container = fw.get(session_container.parents['subject'])
+
+    # Get subject and session names
+    session_label = session_container.label
+    subject_label = subject_container.label
+    prefix = "{}_{}_".format(subject_label, session_label)
+
+    # Inputs
+    ct_image = context.get_input('cortical_thickness_image')
+    ct_image_path = None if ct_image is None else PosixPath(context.get_input_path('cortical_thickness_image'))
+    label_image = context.get_input('label_image')
+    label_image_path = None if label_image is None else PosixPath(context.get_input_path('label_image'))
+
+    # Configs
+    age = config.get('patient_age')
+
+
+def write_command():
+
+    """Write out command script."""
+    with flywheel.GearContext() as context:
+        cmd = ['/usr/local/bin/python',
+            '/opt/scripts/run.py',
+            '--label_index_file {}'.format('/opt/labelset/Schaefer2018_200Parcels_17Networks_order.csv'),
+            '--label_image_file {}'.format(label_image_path),
+            '--ct_image_file {}'.format(ct_image_path),
+            '--patient_age {}'.format(str(age)),
+            '--prefix {}'.format(prefix),
+            '--output_dir {}'.format(gear_output_dir)
+               ]
+    logger.info(' '.join(cmd))
+    # write command joined by spaces
+    with run_script.open('w') as f:
+        f.write(' '.join(cmd))
+
+    return run_script.exists()
+
+
+def main():
+    command_ok = write_command()
+    if not command_ok:
+        logger.warning("Critical error while trying to write run command.")
+        return 1
+    os.system("chmod +x {0}".format(run_script))
+    os.system(run_script)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
